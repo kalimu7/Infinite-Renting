510a8ce44e6a55dfe90af9da7a8072b5c035629f
kalimu7 <mahjourkarim@gmail.com>
2023-04-03 17:46:30 +0000

    working on adding new property

--- JS/animation.js
+++ JS/animation.js
@@ -3,19 +3,38 @@ const tn = document.querySelector('#tenant');
 const ld = document.querySelector('#landlord');
 const frm = document.querySelector('.frm');
 const input = `<input type='hidden' id='key'/>`;
+
+if(tn){
 tn.addEventListener('click',()=>{
-    tn.style.backgroundColor = '#03254b';
-    ld.style.backgroundColor = '#03254bc9';
-    const element = document.getElementById('key');
-    if(element){
-        element.remove();
-    }
-})
+
+        tn.style.backgroundColor = '#03254b';
+        ld.style.backgroundColor = '#03254bc9';
+        const element = document.getElementById('key');
+        if(element){
+            element.remove();
+        }
+    })
+}
+
+if(ld){
 ld.addEventListener('click',()=>{
-    ld.style.backgroundColor = '#03254b';
-    tn.style.backgroundColor = '#03254bc9';
-    const element = document.getElementById('key');
-    if(!element){
-        frm.innerHTML +=input;
-    }
-})
+
+        ld.style.backgroundColor = '#03254b';
+        tn.style.backgroundColor = '#03254bc9';
+        const element = document.getElementById('key');
+        if(!element){
+            frm.innerHTML +=input;
+        }
+    })
+}
+
+// ****open file*********
+const  fileInput = document.querySelector('#fileInput');
+const  bigInput = document.querySelector('#upl');
+bigInput.addEventListener('click', () => {
+    fileInput.click();
+});
+fileInput.addEventListener('change', () => {
+    // Handle the selected file(s) here
+    console.log(fileInput.files);
+});
